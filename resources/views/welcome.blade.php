532de5c110669fe37ab5b85168bae7f422484bf3
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-01-17 15:58:15 +0100

    lil twan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -862,7 +862,7 @@
          src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background"/>
     <div
         class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-        <h1>никита выдренков самый крутой</h1>
+        <h1>type shit</h1>
         <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
             <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                 <div class="flex lg:justify-center lg:col-start-2">
