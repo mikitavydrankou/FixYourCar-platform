56ba04af9e19c9d00697b817ca39026a4a8cdd7e
Mikita Vydrankou <mikitavydrankou@gmail.com>
2025-01-19 15:33:22 +0100

    azaza

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-nav-link :href="route('chats')" :active="request()->routeIs('chats')">
+                {{ __('Chats') }}
+            </x-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
